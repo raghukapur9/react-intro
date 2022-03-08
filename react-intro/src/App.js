9b200c8c40b50bd5222468985ac3df428f2721a4
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2022-03-08 18:24:04 +0530

    added delete and add functionality for tasks

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,13 +1,55 @@
 import './App.css';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+import {useState} from 'react'
 
 function App() {
-  const name = "Raghu";
+  const [tasks, setTasks] = useState(
+    [
+        {   
+            id: 1,
+            text: "Football Match",
+            day: "March 10th 2:30pm",
+            reminder: true,
+        },
+        {   
+            id: 2,
+            text: "Meeting",
+            day: "March 10th 4:30pm",
+            reminder: true,
+        },
+        {   
+            id: 3,
+            text: "Gym",
+            day: "March 10th 8:30pm",
+            reminder: true,
+        }    
+    ]
+  )
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()*10000) + 1;
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  const deleteTask = (id) => {
+    console.log("delete", id);
+    setTasks(tasks.filter((task) => task.id!==id))
+  }
+
+  const toggleReminder = (id) => {
+    console.log("toggle", id);
+    setTasks(tasks.map((task) => task.id===id?{...task, reminder: !task.reminder} : task))
+  }
   return (
     <div className="Container">
       <Header title = "Prop Added"/>
-      <h1> Hello from Activity Tracker</h1>
-      <h2>Greetings from {name}</h2>
+      <AddTask  onAdd={addTask}/>
+      {tasks.length > 0? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onDoubleClick={toggleReminder}/>)
+        : ("No tasks to show") }
     </div>
   );
 }
---
+++ react-intro/src/components/Task.js
@@ -0,0 +1,15 @@
+import {FaTimes} from 'react-icons/fa'
+
+const Task = ({task, onDelete, onDoubleClick}) => {
+    return (
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onDoubleClick(task.id)}>
+            <h3 key = {task.id}>
+                {task.text}<FaTimes style= {{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}/>
+            </h3>
+            <p>{task.day}</p>
+
+        </div>
+    )
+}
+
+export default Task
