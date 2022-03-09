ca7de77c0a8c5d3d263c89af3faec9b8273d7664
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2022-03-09 17:38:13 +0530

    added 'add task' toggle

--- react-intro/src/components/Header.js
+++ react-intro/src/components/Header.js
@@ -1,21 +1,18 @@
 import PropTypes from 'prop-types'
 import Button from './Button';
 
-const Header = (props) => {
-    const onClick = () => {
-        console.log("Added an event");
-    }
+const Header = ({title, onAdd, showAdd}) => {
     return (
         <header className = 'header'>
             <h1>
-                {props.title}
+                {title}
             </h1>
-            <Button color='green' text='Add Task' onClick= {onClick}/>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close Task' : 'Add Task'} onClick= {onAdd}/>
         </header>
     )
 }
 Header.defaultProps = {
-    title : 'Default Title'
+    title : 'Task Tracker'
 }
 
 Header.propTypes = {
